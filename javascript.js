e92dbe56c01c969bc30c5287ebb0b0c3a02db6f1
Luiz Gustavo Valdissera <gvaldissera@gmail.com>
2024-01-23 22:58:40 -0300

    closes #5, closes #6 - history

--- javascript.js
+++ javascript.js
@@ -19,4 +19,14 @@ function calc(op) {
   }
 
   document.getElementById("output").value = output;
+
+  var newHistory =
+    "<p>" + num1 + " " + op + " " + num2 + " = " + output + "</p>";
+  var history = document.getElementById("history");
+
+  history.innerHTML = newHistory + history.innerHTML;
+
+  if (history.children.length > 10) {
+    history.removeChild(history.childNodes[10]);
+  }
 }
